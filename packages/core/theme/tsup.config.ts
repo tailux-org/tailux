67b22a690438b820beaaa6db0d637bab57894386
Emiliano-Murillo <emilianomurillo12@gmail.com>
2024-03-21 13:27:29 -0600

    build(theme): update tsup.config.ts

--- packages/core/theme/tsup.config.ts
+++ packages/core/theme/tsup.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   clean: true,
   target: 'es2019',
-  format: ['cjs', 'esm']
+  format: ['cjs', 'esm'],
+  entry: ['src', '!**/*.test.*']
 })
